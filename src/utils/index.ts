0d505bd43f259c978ee7469ab02c59157bc2f521
Ganiyu Emilandu <52321585+ganiyuemilandu@users.noreply.github.com>
2026-03-02 20:37:39 +0100

    Correct URL to API backend

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,10 +2,10 @@ import axios, { type AxiosRequestConfig } from "axios";
 import reactHotToast from "react-hot-toast";
 
 
-const BASE_URL = import.meta.env.VITE_APP_API_BASE_URL || "http://localhost:3000/api/v1";
+const BASE_URL = import.meta.env.VITE_APP_API_BASE_URL || "http://localhost:3000";
 
 export const axiosClient = axios.create({
-	baseURL: BASE_URL,
+	baseURL: `${BASE_URL}/api/v1`,
 	withCredentials: true
 });
 
